93e0536590f10d4609f79d40a5a121e0ca509c99
Prasad Pathare <prasadsachinist10@gmail.com>
2025-02-07 07:12:30 +0000

    add hero UI

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,13 @@
+"use client";
+import { Button, HeroUIProvider } from "@heroui/react";
+
 export default function Home() {
-  return <h1>Chatify</h1>;
+  return (
+    <HeroUIProvider>
+      <div className="min-h-screen max-h-screen grid place-items-center">
+        <h1>Chatify</h1>
+        <Button>Click me</Button>
+      </div>
+    </HeroUIProvider>
+  );
 }
